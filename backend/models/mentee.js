2f1b95ef95da376946e92e743db27a88a658035a
nikhilg777 <nikhil.g777@gmail.com>
2020-03-16 19:52:53 -0400

    added fields in Mentor, Mentee Models

--- backend/models/mentee.js
+++ backend/models/mentee.js
@@ -5,7 +5,9 @@ const Mentee = new Schema(
     {
         firstName: {type: String, required: true},
         lastName: {type: String, required: true},
-        email: {type: String, required: true}
+        email: {type: String, required: true},
+        title: {type: String, required: true},
+        description: {type: String, required: true}
     },
     {
         timestamps: true
