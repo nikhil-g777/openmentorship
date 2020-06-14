73e0f2f50469d72b5aeb0af5d8d5ef452e4b0f36
Olessia Potapova <opotapova@adfi.co>
2020-06-14 19:52:12 -0300

    register main page part 2

--- website/src/app/index.js
+++ website/src/app/index.js
@@ -1,27 +1,30 @@
 
 import React from 'react'
+import { CreateMentee, CreateMentor, FAQ, Home, RegisterMain } from '../pages'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
-import { NavBar } from '../components'
-import { CreateMentee, CreateMentor, FAQ, Home } from '../pages'
-
-
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { theme }  from "./GlobalTheme"
+
 function App() {
-    return (
-        <div>
-        <Router>
-            <NavBar />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path='/mentees/create' component={CreateMentee} />
-              <Route path='/mentors/create' component={CreateMentor} />
-              <Route path="/FAQ" exact component={FAQ}/>
-            </Switch>
-        </Router>
-        </div>
-    )
+  return (
+      <>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path='/mentees/create' component={CreateMentee} />
+            <Route path='/mentors/create' component={CreateMentor} />
+            <Route path="/FAQ" exact component={FAQ}/>
+            <Route path="/register" component={RegisterMain} />
+          </Switch>
+        </ThemeProvider>
+      </Router>
+      </>
+  )
 };
 
 export default App;
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -2,10 +2,12 @@ import CreateMentee from './CreateMentee'
 import CreateMentor from './CreateMentor'
 import FAQ  from '../pages/FAQ/FAQ'
 import Home from '../pages/Home'
+import RegisterMain from './Register/RegisterMain'
 
 export {
     CreateMentee,
     CreateMentor,
     FAQ,
-    Home
+    Home,
+    RegisterMain
 }
