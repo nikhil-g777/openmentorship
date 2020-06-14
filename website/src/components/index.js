90e9b214e8cf5cde68db095837a92ad66a5ca806
Olessia Potapova <opotapova@adfi.co>
2020-06-14 20:02:17 -0300

    trying to get rid of packages

--- website/src/components/index.js
+++ website/src/components/index.js
@@ -2,5 +2,6 @@
 import Links from './Links'
 import Logo from './Logo'
 import NavBar from './NavBar'
+import SignUp from './SignUp'
 
-export { Links, Logo, NavBar }
+export { Links, Logo, NavBar, SignUp }
