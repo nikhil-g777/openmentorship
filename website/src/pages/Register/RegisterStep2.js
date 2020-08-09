d66a970e240be5c1dd0e5ca10c93c8bb28fe6b50
Olessia Potapova <opotapova@adfi.co>
2020-08-09 11:58:22 -0300

    cleaned up work experience, saving 1 job now, took out LI button

--- website/src/pages/Register/RegisterStep2.js
+++ website/src/pages/Register/RegisterStep2.js
@@ -16,7 +16,7 @@ const RegisterStep2 = props => {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   let years = [2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000]
   
-  return (
+  return ( 
     <Container>
       <TitleWrapper>
         <BackButton handleBack={props.handleBack} />
