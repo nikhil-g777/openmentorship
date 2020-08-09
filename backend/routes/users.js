5de971b19470df13e828fbef075c8b8fdebc9704
nikhil gumidelli <nikhilgumidelli@nikhils-MacBook-Pro.local>
2020-08-08 23:46:59 -0400

    updated APIs and using _id

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var userController = require('../controllers/userController');
 
 router.post('/register', userController.registerUser);
-router.put('/update', userController.updateUser);
+router.put('/update/:_id', userController.updateUser);
+router.get('/info/:_id', userController.userInfo);
+router.get('/matches/:_id', userController.matches);
 
 module.exports = router;
