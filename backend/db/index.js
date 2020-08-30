472895adef35fac3242db38f24fb05cd7a877594
nikhil gumidelli <nikhilgumidelli@nikhils-mbp.home>
2020-08-29 22:29:34 -0400

    fixed mongo uri

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGODB_CONNECTION_URL, {
+  .connect(process.env.MONGODB_CONNECTION_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
