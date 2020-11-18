9b3f79ada9b84db94d40cce5f19b7f9ba85e2ecb
nikhil gumidelli <nikhilgumidelli@nikhils-MacBook-Pro.local>
2020-11-18 02:51:36 -0500

    updating to header based auth

--- backend/config/passportJWT.js
+++ backend/config/passportJWT.js
@@ -4,20 +4,32 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const PUBLIC_KEY = fs.readFileSync(`${__dirname}/../keys/public.pem`);
 const User = require('../models/user');
 
-const cookieExtractor = (req) => {
-  console.log(req.headers);
-  let token = null;
+// Commenting for now, but can be used if we switch to cookies
+// const cookieExtractor = (req) => {
+//   console.log(req.headers);
+//   let token = null;
 
-  if (req && req.cookies) {
-    token = req.cookies.accessToken; // authorization Bearer token
+//   if (req && req.cookies) {
+//     token = req.cookies.accessToken; // authorization Bearer token
+//   }
+
+//   return token;
+// };
+
+const headerTokenExtractor = (req) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    return token;
   }
-
-  return token;
+  return null;
 };
 
 const options = {
   secretOrKey: PUBLIC_KEY,
-  jwtFromRequest: cookieExtractor,
+  jwtFromRequest: headerTokenExtractor,
   algorithms: 'RS256',
 };
 
