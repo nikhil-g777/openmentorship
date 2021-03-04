0b04d7ece623233b61db27f568723381dfc7d557
nikhil gumidelli <nikhilgumidelli@nikhils-MacBook-Pro.local>
2021-03-03 20:19:05 -0500

    updates for waitlist

--- website/src/api/index.js
+++ website/src/api/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
-  withCredentials: true,
 });
 
 const getToken = () => JSON.parse(localStorage.getItem("token"));
