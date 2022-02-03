21123fec07a9d7e98c7f154b476d2952d6f6b7d4
zainnasir138 <zainnasir138@gmail.com>
2022-02-03 14:52:47 +0500

    admin apis integration

--- website/src/admin/dashboard/Dashboard.js
+++ website/src/admin/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Container, Tab, Tabs } from "@material-ui/core";
+import { Box, Tab, Tabs } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import { Menu1 } from "../Component";
