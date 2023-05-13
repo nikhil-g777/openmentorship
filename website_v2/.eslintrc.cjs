9432ef50f1fc5d18f6eab59854057023a4cb7340
helloukey <kunalukey32@gmail.com>
2023-05-13 17:21:45 +0530

    added eslint and prettier with pre-commit hook.

--- website_v2/.eslintrc.cjs
+++ website_v2/.eslintrc.cjs
@@ -1,15 +1,24 @@
 module.exports = {
   env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-    "@typescript-eslint/no-var-requires": 0
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./website_v2/tsconfig.json",
   },
-}
+  plugins: ["react", "@typescript-eslint", "react-refresh", "prettier"],
+  rules: {
+    "@typescript-eslint/quotes": ["error", "double"],
+    "react/react-in-jsx-scope": 0,
+    "react-refresh/only-export-components": "warn",
+    "@typescript-eslint/no-var-requires": 0,
+  },
+};
--- website_v2/src/App.tsx
+++ website_v2/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <h1 className="text-center">Hello Frontend</h1>
-    </>
+    <h1>
+      Hello Frontend
+    </h1>
   );
 }
 
