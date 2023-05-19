34832b9cf57db107a5e5ad3732b1a8f5659d9db7
helloukey <kunalukey32@gmail.com>
2023-05-19 21:02:37 +0530

    added redux/redux-toolkit boilerplate with counter slice

--- website_v2/src/pages/landing/index.tsx
+++ website_v2/src/pages/landing/index.tsx
@@ -1,19 +1,19 @@
-import DreamCareer from './DreamCareer'
-import Hero from './Hero'
-import HowItWorks from './HowItWorks'
-import WhyOpenMentorship from './WhyOpenMentorship'
+import DreamCareer from "./DreamCareer";
+import Hero from "./Hero";
+import HowItWorks from "./HowItWorks";
+import WhyOpenMentorship from "./WhyOpenMentorship";
 
-type Props = {}
+type Props = {};
 
 const Landing = (props: Props) => {
   return (
     <div className="w-full">
-        <Hero />
-        <HowItWorks />
-        <WhyOpenMentorship />
-        <DreamCareer />
+      <Hero />
+      <HowItWorks />
+      <WhyOpenMentorship />
+      <DreamCareer />
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
