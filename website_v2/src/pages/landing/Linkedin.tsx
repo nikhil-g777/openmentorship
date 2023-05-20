34d625305cb0ea3e7c98ef3388c9567f53b78360
helloukey <kunalukey32@gmail.com>
2023-05-20 20:27:09 +0530

    added scope field in Linkedin component

--- website_v2/src/pages/landing/Linkedin.tsx
+++ website_v2/src/pages/landing/Linkedin.tsx
@@ -3,19 +3,24 @@ import { useLinkedIn } from "react-linkedin-login-oauth2";
 // You can use provided image shipped by this package or using your own
 import linkedin from "react-linkedin-login-oauth2/assets/linkedin.png";
 import SuccessModal from "../../components/modals/SuccessModal";
+import { useLoginUserMutation } from "../../redux/apis/userApi";
 
 const Linkedin = () => {
   const [isModal, setIsModal] = useState<boolean>(false);
   const [heading, setHeading] = useState<string>("");
   const [message, setMessage] = useState<string>("");
 
+  const [loginUser, result] = useLoginUserMutation();
+
   const { linkedInLogin } = useLinkedIn({
     clientId: import.meta.env.VITE_LINKEDIN_CLIENT_ID,
     redirectUri: import.meta.env.VITE_LINKEDIN_REDIRECT_URI,
+    scope: "r_emailaddress r_liteprofile",
     onSuccess: (code) => {
       setIsModal(true);
       setHeading("Your LinkedIn auth code:");
       setMessage(code);
+      loginUser({authCode: code, isLocal: true});
     },
     onError: (error) => {
       console.log(error);
