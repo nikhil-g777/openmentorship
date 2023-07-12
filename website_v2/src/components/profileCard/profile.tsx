c6843d3a55583818bec59cf39440662ef5404ce5
helloukey <kunalukey32@gmail.com>
2023-07-12 23:40:45 +0530

    added back profileCard component folder

---
+++ website_v2/src/components/profileCard/profile.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Image from "next/image";
+import {useState} from "react";
+
+type Props = {
+  data: {
+    [key: string]: string;
+  };
+  buttonText: string;
+};
+const Profile = ({data, buttonText}: Props) => {
+  const [fallbackSrc, setFallbackSrc] = useState<string | null>(null);
+
+  return (
+    <div className="flex flex-col">
+      <Image
+        src={
+          data && data.default && !fallbackSrc
+            ? data.default
+            : "/assets/icons/profile.svg"
+        }
+        onError={() => setFallbackSrc("/assets/icons/profile.svg")}
+        alt="random-user"
+        width={200}
+        height={200}
+        className="w-full max-w-[200px] h-auto rounded-md mx-auto object-cover"
+      />
+      <button className="w-full max-w-[200px] btn btn-accent rounded-full btn-sm mt-4 mx-auto text-sm capitalize">
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+export {Profile};
