56aa1274dfbd4edd1f004136f04797c4af9ed082
helloukey <kunalukey32@gmail.com>
2023-07-13 17:10:40 +0530

    added trycatch block for explore endpoints

--- website_v2/src/endpoints/explore.ts
+++ website_v2/src/endpoints/explore.ts
@@ -2,14 +2,18 @@ import {headerProvider} from "./root";
 
 // Get Explore Data (User Recommendations)
 const getExploreData = async (token: string) => {
-  const res = await fetch(
-    `${process.env.BACKEND_BASE_URL}/matches/userRecommendations`,
-    {
-      headers: headerProvider(token),
-    }
-  );
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(
+      `${process.env.BACKEND_BASE_URL}/matches/userRecommendations`,
+      {
+        headers: headerProvider(token),
+      }
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 // Get Explore Content (Mentors list)
@@ -22,19 +26,19 @@ const getExploreDataByContent = async (
   communicationFrequency: string,
   communicationPreferences: string
 ) => {
-  const res = await fetch(
-    `${process.env.BACKEND_BASE_URL}/matches/searchMentors?page=${page}&limit=${limit}&areasOfInterest=${areasOfInterest}&goals=${goals}&communicationFrequency=${communicationFrequency}&communicationPreferences=${communicationPreferences}`,
-    {
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-        accept: "application/json",
-        authorization: `Bearer ${token}`,
-      },
-      cache: "no-store",
-    }
-  );
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(
+      `${process.env.BACKEND_BASE_URL}/matches/searchMentors?page=${page}&limit=${limit}&areasOfInterest=${areasOfInterest}&goals=${goals}&communicationFrequency=${communicationFrequency}&communicationPreferences=${communicationPreferences}`,
+      {
+        headers: headerProvider(token),
+        cache: "no-store",
+      }
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 export {getExploreData, getExploreDataByContent};
