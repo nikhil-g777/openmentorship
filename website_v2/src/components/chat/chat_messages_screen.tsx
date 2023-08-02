0e8814fb28338eac02d801217586bcad0cba0b53
helloukey <kunalukey32@gmail.com>
2023-08-02 17:19:15 +0530

    added view more messages feature based on first message in view

--- website_v2/src/components/chat/chat_messages_screen.tsx
+++ website_v2/src/components/chat/chat_messages_screen.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import {getTime} from "@/helpers/chat";
 import {useChatStore} from "@/zustand/store";
 import {useSession} from "next-auth/react";
 import {MutableRefObject, useEffect, useRef} from "react";
@@ -14,6 +15,7 @@ const ChatMessagesScreen = () => {
       element.current.scrollTop = element.current?.scrollHeight;
     }
   };
+  const chatContainer = useRef<null | HTMLDivElement>(null);
 
   useEffect(() => {
     // Reset conversations
@@ -35,13 +37,34 @@ const ChatMessagesScreen = () => {
     });
   }, [currentConversation]);
 
+  useEffect(() => {
+    // Trigger prevPage when first message is in view
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && conversations?.hasPrevPage) {
+        conversations.prevPage().then(messages => {
+          // add messages to conversations at the beginning
+          setConversations({
+            ...messages,
+            items: [...messages.items, ...conversations.items],
+          });
+        });
+      }
+    });
+    if (chatContainer.current && chatContainer.current.firstElementChild) {
+      observer.observe(chatContainer.current.firstElementChild);
+    }
+
+    // cleanup function
+    return () => observer.disconnect();
+  }, [conversations, setConversations]);
+
   return (
     <div className="w-full overflow-auto" ref={scrollElement}>
       {chatId &&
       chatId.length &&
       conversations &&
       conversations?.items?.length ? (
-        <div className="w-full p-4 flex flex-col last:mb-8">
+        <div className="w-full p-4 flex flex-col last:mb-8" ref={chatContainer}>
           {conversations.items.map(item => (
             <div
               key={item["state"]["sid"]}
@@ -57,6 +80,11 @@ const ChatMessagesScreen = () => {
                 }`}
               >
                 {item["state"]["body"]}
+              </div>
+              <div className="chat-footer opacity-50">
+                <time className="text-xs">
+                  {getTime(item["state"]["timestamp"])}
+                </time>
               </div>
             </div>
           ))}
---
+++ website_v2/src/helpers/chat.ts
@@ -0,0 +1,11 @@
+// Get Time in AM/PM format
+const getTime = (date: Date) => {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  const formattedHours = hours % 12 || 12;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  return `${formattedHours}:${formattedMinutes} ${ampm}`;
+};
+
+export {getTime};
