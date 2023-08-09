c95f9ab67d29d1735cc15986d09729f820945b69
helloukey <kunalukey32@gmail.com>
2023-08-09 18:56:33 +0530

    added loader for router actions

--- website_v2/src/app/explore/loading.tsx
+++ website_v2/src/app/explore/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="w-full h-full flex items-center justify-center mt-20">
-      <button className="btn btn-square loading"></button>
+      <button className="btn btn-primary btn-square loading"></button>
     </div>
   );
 };
--- website_v2/src/components/filter/filter_main.tsx
+++ website_v2/src/components/filter/filter_main.tsx
@@ -5,7 +5,9 @@ import {
   performParamsToArray,
   performUrlFormat,
 } from "@/helpers/filter";
+import {RouteActionLoader} from "@/loaders/route_action_loader";
 import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {useTransition} from "react";
 
 type Filters = {
   label: string;
@@ -35,6 +37,7 @@ const Filter = ({heading, filters, type}: Props) => {
     "communicationPreferences",
     params
   );
+  const [isPending, startTransition] = useTransition();
 
   const listing = [
     ...areasOfInterest,
@@ -64,39 +67,44 @@ const Filter = ({heading, filters, type}: Props) => {
 
     // Push to the new URL
     const uri = `${pathname}?page=${1}&limit=${limit}&areasOfInterest=${aoiUrl}&goals=${goalsUrl}&communicationFrequency=${cfUrl}&communicationPreferences=${cpUrl}`;
-    router.push(uri);
-    router.refresh();
+    startTransition(() => {
+      router.push(uri);
+      router.refresh();
+    });
   };
 
   return (
-    <div className="w-full max-w-6xl mx-auto">
-      <div className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
-        <input type="checkbox" className="peer" />
-        <div className="collapse-title bg-white text-black peer-checked:text-black">
-          {heading}
-        </div>
-        <div className="collapse-content bg-white text-black peer-checked:text-black">
-          {/* Filters */}
-          {filters && filters.length
-            ? filters.map(filter => (
-                <div className="form-control" key={filter.key}>
-                  <label htmlFor={filter.id} className="label cursor-pointer">
-                    <span className="label-text">{filter.label}</span>
-                    <input
-                      id={filter.id}
-                      type="checkbox"
-                      name={filter.text}
-                      className="checkbox"
-                      onChange={handleFilter}
-                      checked={listing.includes(filter.text)}
-                    />
-                  </label>
-                </div>
-              ))
-            : null}
+    <>
+      {isPending ? <RouteActionLoader /> : null}
+      <div className="w-full max-w-6xl mx-auto">
+        <div className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-white text-black peer-checked:text-black">
+            {heading}
+          </div>
+          <div className="collapse-content bg-white text-black peer-checked:text-black">
+            {/* Filters */}
+            {filters && filters.length
+              ? filters.map(filter => (
+                  <div className="form-control" key={filter.key}>
+                    <label htmlFor={filter.id} className="label cursor-pointer">
+                      <span className="label-text">{filter.label}</span>
+                      <input
+                        id={filter.id}
+                        type="checkbox"
+                        name={filter.text}
+                        className="checkbox checkbox-primary"
+                        onChange={handleFilter}
+                        checked={listing.includes(filter.text)}
+                      />
+                    </label>
+                  </div>
+                ))
+              : null}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
