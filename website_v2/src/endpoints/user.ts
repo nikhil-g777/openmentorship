78c0f2dcdb118f92c27d5c8353c692fd7f5dea7c
nikhil-g777 <nikhil.g777@gmail.com>
2023-08-22 22:28:47 +0530

    removing print logs

--- website_v2/src/endpoints/user.ts
+++ website_v2/src/endpoints/user.ts
@@ -20,8 +20,6 @@ const getUserInfo = async (authCode: string) => {
 const nextAuthLogin = async (
   credentials: Record<"authCode", string> | undefined
 ) => {
-  console.log("!!!");
-  console.log(process.env.NEXT_APP_IS_LOCAL);
   try {
     const res = await fetch(`${process.env.BACKEND_BASE_URL}/users/login`, {
       method: "POST",
