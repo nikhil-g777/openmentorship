fedbd11117f1fb896ba80b136d9acb645ab37483
helloukey <kunalukey32@gmail.com>
2023-08-23 14:31:58 +0530

    added loaders for admin dashboard page

--- website_v2/src/loaders/admin/dashboard/users_loader.tsx
+++ website_v2/src/loaders/admin/dashboard/users_loader.tsx
@@ -2,17 +2,17 @@ import {ProfileCardLoader} from "@/loaders/profile_card_loader";
 
 const UsersLoader = () => {
   return (
-    <div className="w-full py-16">
+    <div className="w-full">
       {/* Users Filter */}
       <div
-        className="w-full border border-base-300 rounded-box animate-pulse"
+        className="w-full bg-base-300 border border-base-300 rounded-box animate-pulse"
         style={{width: "100%", height: "120px"}}
       ></div>
       {/* Users Container */}
       <div className="w-full pt-20">
         {/* Header Header */}
         <div
-          className="animate-pulse mb-4"
+          className="bg-base-300 animate-pulse mb-4"
           style={{width: "128px", height: "24px"}}
         ></div>
         {/* Users */}
@@ -21,6 +21,13 @@ const UsersLoader = () => {
         <ProfileCardLoader />
         <ProfileCardLoader />
         <ProfileCardLoader />
+        {/* Pagination */}
+        <div className="w-full btn-group mb-24 flex justify-center">
+          <div
+            className="bg-base-300 border-base-300 animate-pulse btn pointer-events-none"
+            style={{width: "160px", height: "48px"}}
+          ></div>
+        </div>
       </div>
     </div>
   );
