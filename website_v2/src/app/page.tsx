f273d29e4b1d4aa3ae24b2f07306386f6c457046
helloukey <kunalukey32@gmail.com>
2023-08-24 13:06:34 +0530

    added faq list in helpers folder

--- website_v2/src/app/page.tsx
+++ website_v2/src/app/page.tsx
@@ -3,44 +3,7 @@ import {DreamCareer} from "@/components/landing/dream_career";
 import {Hero} from "@/components/landing/hero_main";
 import {HowItWorks} from "@/components/landing/how_it_works";
 import {WhyOpenMentorship} from "@/components/landing/why_open_mentorship";
-
-const list = [
-  {
-    id: "1",
-    heading:
-      "This is the most frequent question asked about this experience. does this help?",
-    description:
-      "This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq.",
-  },
-  {
-    id: "2",
-    heading:
-      "This is the most frequent question asked about this experience. does this help?",
-    description:
-      "This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq.",
-  },
-  {
-    id: "3",
-    heading:
-      "This is the most frequent question asked about this experience. does this help?",
-    description:
-      "This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq.",
-  },
-  {
-    id: "4",
-    heading:
-      "This is the most frequent question asked about this experience. does this help?",
-    description:
-      "This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq.",
-  },
-  {
-    id: "5",
-    heading:
-      "This is the most frequent question asked about this experience. does this help?",
-    description:
-      "This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq.",
-  },
-];
+import {list} from "@/helpers/landing";
 
 export default function Home() {
   return (
---
+++ website_v2/src/helpers/landing.ts
@@ -0,0 +1,39 @@
+const list = [
+  {
+    id: "1",
+    heading:
+      "This is the most frequent question asked about this experience. does this help?",
+    description:
+      "This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq.",
+  },
+  {
+    id: "2",
+    heading:
+      "This is the most frequent question asked about this experience. does this help?",
+    description:
+      "This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq.",
+  },
+  {
+    id: "3",
+    heading:
+      "This is the most frequent question asked about this experience. does this help?",
+    description:
+      "This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq.",
+  },
+  {
+    id: "4",
+    heading:
+      "This is the most frequent question asked about this experience. does this help?",
+    description:
+      "This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq.",
+  },
+  {
+    id: "5",
+    heading:
+      "This is the most frequent question asked about this experience. does this help?",
+    description:
+      "This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq. This is the sample response to the to the faq.",
+  },
+];
+
+export {list};
