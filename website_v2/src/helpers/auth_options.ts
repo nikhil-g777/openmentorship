73dd03986554afd2ee81197595810b8c1bfd21a8
helloukey <kunalukey32@gmail.com>
2023-08-25 17:31:50 +0530

    added response success check for login

--- website_v2/src/helpers/auth_options.ts
+++ website_v2/src/helpers/auth_options.ts
@@ -26,7 +26,7 @@ export const authOptions: NextAuthOptions = {
         const user = await res.json();
 
         // If no error and we have user data, return it
-        if (res.ok && user) {
+        if (res.ok && user.success) {
           return user;
         }
         // Return null if user data could not be retrieved
