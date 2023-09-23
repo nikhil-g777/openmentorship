9c3be8daf966be996d5893ef7c2f8f474317d0c7
helloukey <kunalukey32@gmail.com>
2023-09-23 15:44:08 +0530

    refactored chat attachment modal

--- website_v2/src/app/chat/page.tsx
+++ website_v2/src/app/chat/page.tsx
@@ -1,6 +1,6 @@
 import {ChatWrapper} from "@/components/chat/chat_wrapper";
 import {StoreInitializer} from "@/components/chat/store_initializer";
-import {ChatAttachmentModal} from "@/components/modals/chat_attachment_modal";
+import {ChatAttachmentModal} from "@/components/modals/attachment/chat_attachment_modal";
 import {ChatMediaContentModal} from "@/components/modals/chat_media_content_modal";
 import {NoResult} from "@/components/noResult/no_result";
 import {getChatToken} from "@/endpoints/chat";
