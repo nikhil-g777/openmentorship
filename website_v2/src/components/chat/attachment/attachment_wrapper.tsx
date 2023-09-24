63ef962487e1d0619acc2f4c85a9e9c6406940f0
helloukey <kunalukey32@gmail.com>
2023-09-24 15:14:42 +0530

    added placeholder dataURL for image

--- website_v2/src/components/chat/attachment/attachment_wrapper.tsx
+++ website_v2/src/components/chat/attachment/attachment_wrapper.tsx
@@ -10,6 +10,9 @@ type Props = {
   message: Message;
 };
 
+const dataURL =
+  "data:image/webp;base64,UklGRgACAABXRUJQVlA4WAoAAAAgAAAANgMANgMASUNDUMgBAAAAAAHIAAAAAAQwAABtbnRyUkdCIFhZWiAH4AABAAEAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAACRyWFlaAAABFAAAABRnWFlaAAABKAAAABRiWFlaAAABPAAAABR3dHB0AAABUAAAABRyVFJDAAABZAAAAChnVFJDAAABZAAAAChiVFJDAAABZAAAAChjcHJ0AAABjAAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEJYWVogAAAAAAAAb6IAADj1AAADkFhZWiAAAAAAAABimQAAt4UAABjaWFlaIAAAAAAAACSgAAAPhAAAts9YWVogAAAAAAAA9tYAAQAAAADTLXBhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABtbHVjAAAAAAAAAAEAAAAMZW5VUwAAACAAAAAcAEcAbwBvAGcAbABlACAASQBuAGMALgAgADIAMAAxADZWUDhMEQAAAC82g80AB9CZStSl/4GI6H8AAA==";
+
 const AttachmentWrapper = ({
   contentType,
   src,
@@ -22,46 +25,44 @@ const AttachmentWrapper = ({
       {/* Image */}
       {contentType.startsWith("image") ? (
         <a href={src} target="_blank">
-          <div style={{width: "150px", height: "150px", position: "relative"}}>
-            <Image
-              unoptimized
-              src={src}
-              alt={contentName}
-              fill
-              className="object-cover"
-              onError={handleError}
-            />
-          </div>
+          <Image
+            unoptimized
+            src={src}
+            placeholder="blur"
+            blurDataURL={dataURL}
+            alt={contentName}
+            width={100}
+            height={100}
+            className="object-cover"
+            onError={handleError}
+          />
         </a>
       ) : null}
       {/* Video */}
       {contentType.startsWith("video") ? (
         <a href={src} target="_blank">
-          <div style={{width: "150px", height: "150px", position: "relative"}}>
-            <video
-              src={src}
-              width={100}
-              height={100}
-              controls
-              onError={handleError}
-            >
-              Your browser does not support the video tag.
-            </video>
-          </div>
+          <video
+            src={src}
+            poster={dataURL}
+            width={100}
+            height={100}
+            controls
+            onError={handleError}
+          >
+            Your browser does not support the video tag.
+          </video>
         </a>
       ) : null}
       {/* Documents */}
       {!contentType.startsWith("image") && !contentType.startsWith("video") ? (
         <a href={src} target="_blank">
-          <div style={{width: "150px", height: "150px", position: "relative"}}>
-            <Image
-              unoptimized
-              src="/assets/icons/document.svg"
-              alt={contentName}
-              fill
-              className="bg-white object-cover rounded-md"
-            />
-          </div>
+          <Image
+            src="/assets/icons/document.svg"
+            width={100}
+            height={100}
+            alt={contentName}
+            className="bg-white object-cover rounded-md"
+          />
         </a>
       ) : null}
       {/* File Name */}
