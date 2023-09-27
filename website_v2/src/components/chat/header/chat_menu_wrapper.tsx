b25a1762a284e8394ccda4373efa038a3f52b9dd
helloukey <kunalukey32@gmail.com>
2023-09-27 15:52:57 +0530

    added desktop menu button and removed end sesssion icon

--- website_v2/src/components/chat/header/chat_menu_wrapper.tsx
+++ website_v2/src/components/chat/header/chat_menu_wrapper.tsx
@@ -36,43 +36,70 @@ const ChatMenuWrapper = () => {
     setChatIndicator(false);
   };
   return (
-    <div className="indicator">
-      {chatIndicator ? (
-        <span className="indicator-item badge badge-xs badge-primary"></span>
-      ) : null}
-      <div className="dropdown dropdown-end">
-        <label tabIndex={0} className="btn btn-sm btn-ghost btn-circle">
-          <Image
-            src="/assets/icons/dots.svg"
-            alt="chat-menu"
-            width={24}
-            height={24}
-          />
-        </label>
-        <ul
-          tabIndex={0}
-          className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+    <>
+      {/* Media Button for Desktop */}
+      <div className="indicator hidden md:block mr-4 mt-[2px]">
+        {chatIndicator ? (
+          <span className="indicator-item badge badge-xs badge-primary"></span>
+        ) : null}
+        {/* button */}
+        <button
+          onClick={handleMedia}
+          className="btn btn-ghost btn-xs flex-nowrap items-center gap-1.5 px-4"
         >
-          <li onClick={handleMedia}>
-            <button className="justify-between">
-              Media
-              {chatIndicator ? (
-                <span className="badge badge-primary">New</span>
-              ) : null}
-            </button>
-          </li>
-          <li>
-            <Link href="/chat">Chat Home</Link>
-          </li>
-          <li
-            onClick={handleEndSession}
-            className={chatType === "archive" ? "disabled" : ""}
-          >
-            <button className="hover:bg-error">End Session</button>
-          </li>
-        </ul>
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="media"
+            width={20}
+            height={20}
+          />
+          Media
+        </button>
       </div>
-    </div>
+
+      {/* Menu Container */}
+      <div className="indicator">
+        {chatIndicator ? (
+          <span className="indicator-item badge badge-xs badge-primary md:hidden"></span>
+        ) : null}
+        <div className="dropdown dropdown-end">
+          {/* Menu */}
+          <label tabIndex={0} className="btn btn-sm btn-ghost btn-circle">
+            <Image
+              src="/assets/icons/dots.svg"
+              alt="chat-menu"
+              width={24}
+              height={24}
+            />
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            {/* Media */}
+            <li onClick={handleMedia} className="md:hidden">
+              <button className="justify-between">
+                Media
+                {chatIndicator ? (
+                  <span className="badge badge-primary">New</span>
+                ) : null}
+              </button>
+            </li>
+            {/* Chat Home */}
+            <li>
+              <Link href="/chat">Chat Home</Link>
+            </li>
+            {/* End Session */}
+            <li
+              onClick={handleEndSession}
+              className={chatType === "archive" ? "disabled" : ""}
+            >
+              <button className="hover:bg-error">End Session</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
