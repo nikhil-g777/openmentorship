c015578f4fa3823692d79a7475b8a7e4ef070769
helloukey <kunalukey32@gmail.com>
2023-09-28 00:25:29 +0530

    added condition to show discover page link to only mentee

--- website_v2/src/components/layout/navbar/navbar_links.tsx
+++ website_v2/src/components/layout/navbar/navbar_links.tsx
@@ -1,7 +1,9 @@
+import {useSession} from "next-auth/react";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 
 const NavbarLinks = () => {
+  const userType = useSession()?.data?.user?.user?.userType || "";
   const pathname = usePathname();
 
   return (
@@ -23,12 +25,14 @@ const NavbarLinks = () => {
         </>
       ) : (
         <>
-          <Link
-            href="/explore?page=1&limit=10&areasOfInterest=&goals=&communicationFrequency=&communicationPreferences="
-            className="btn btn-ghost normal-case text-base"
-          >
-            Discover
-          </Link>
+          {userType === "mentee" ? (
+            <Link
+              href="/explore?page=1&limit=10&areasOfInterest=&goals=&communicationFrequency=&communicationPreferences="
+              className="btn btn-ghost normal-case text-base"
+            >
+              Discover
+            </Link>
+          ) : null}
           <Link href="/matches" className="btn btn-ghost normal-case text-base">
             Matches
           </Link>
