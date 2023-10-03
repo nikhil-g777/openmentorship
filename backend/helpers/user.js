6240cc7cdc6779545371edb4c09da72105711bc2
helloukey <kunalukey32@gmail.com>
2023-10-03 23:34:07 +0530

    added try catch block for handleUserRegistration helper function

--- backend/helpers/user.js
+++ backend/helpers/user.js
@@ -7,62 +7,78 @@ const constants = require('../lib/constants');
 const handleUserRegistration = async (req, res, linkedInProfile) => {
   const { body } = req;
 
-  if (body.type == 'linkedInSignup') {
-    if (!body.authCode) {
-      return res
-        .status(400)
-        .json({ success: false, error: 'authCode missing in the body' });
-    }
-    const results = {};
+  try {
+    if (body.type == 'linkedInSignup') {
+      if (!body.authCode) {
+        return res
+          .status(400)
+          .json({ success: false, error: 'authCode missing in the body' });
+      }
 
-    results.linkedInProfile = linkedInProfile;
-    const user = await User.findOne({
-      linkedInId: linkedInProfile.linkedInId,
-    }).exec();
-    let userObj = user;
-    if (user) {
-      userObj = Object.assign(user, results.linkedInProfile);
-    } else {
-      userObj = await new User(results.linkedInProfile);
-    }
-    userObj.profileImageUrls = results.linkedInProfile.profileImageUrls;
-    const updatedUser = await userObj.save();
-    results.updatedUser = updatedUser;
-    const token = await Token.findOne({ userId: updatedUser._id });
-    // if token isn't in our DB, store
-    if (!token) {
-      // encrypt information
-      const t = util.refreshToken(results.updatedUser._id);
+      // Results object to store the results of the registration
+      const results = {};
+      results.linkedInProfile = linkedInProfile;
 
-      Token.create({
-        refreshToken: t,
-        userId: results.updatedUser._id,
-      });
+      // Check if user already exists
+      const user = await User.findOne({
+        linkedInId: linkedInProfile.linkedInId,
+      }).exec();
+
+      // If user exists, assign linkedInProfile to user or create new user
+      let userObj = user;
+      if (user) {
+        userObj = Object.assign(user, results.linkedInProfile);
+      } else {
+        userObj = await new User(results.linkedInProfile);
+      }
+
+      // Add profile image urls and save user
+      userObj.profileImageUrls = results.linkedInProfile.profileImageUrls;
+      const updatedUser = await userObj.save();
+
+      // Store the updated user in results and send the access token
+      results.updatedUser = updatedUser;
+      const token = await Token.findOne({ userId: updatedUser._id });
+      // if token isn't in our DB, store
+      if (!token) {
+        // encrypt information
+        const t = util.refreshToken(results.updatedUser._id);
+
+        Token.create({
+          refreshToken: t,
+          userId: results.updatedUser._id,
+        });
+      }
+      // send the access token
+      const accessToken = util.accessToken(results.updatedUser._id);
+      return (
+        res
+          // .cookie('accessToken', accessToken, {
+          //   sameSite: 'none',
+          //   secure: true,
+          // })
+          .status(401)
+          .json({
+            success: false,
+            newUser: true,
+            error: constants.loginMessageByStatus.incomplete,
+            registrationStatus: constants.registrationStatus.incomplete,
+            token: accessToken,
+            user: {
+              _id: results.updatedUser._id,
+              firstName: results.linkedInProfile.firstName,
+              lastName: results.linkedInProfile.lastName,
+              linkedInId: results.linkedInProfile.linkedInId,
+              email: results.linkedInProfile.email,
+            },
+          })
+      );
     }
-    // send the access token
-    const accessToken = util.accessToken(results.updatedUser._id);
-    return (
-      res
-        // .cookie('accessToken', accessToken, {
-        //   sameSite: 'none',
-        //   secure: true,
-        // })
-        .status(401)
-        .json({
-          success: false,
-          newUser: true,
-          error: constants.loginMessageByStatus.incomplete,
-          registrationStatus: constants.registrationStatus.incomplete,
-          token: accessToken,
-          user: {
-            _id: results.updatedUser._id,
-            firstName: results.linkedInProfile.firstName,
-            lastName: results.linkedInProfile.lastName,
-            linkedInId: results.linkedInProfile.linkedInId,
-            email: results.linkedInProfile.email,
-          },
-        })
-    );
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Unable to register user',
+    });
   }
 };
 
