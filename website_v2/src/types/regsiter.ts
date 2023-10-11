d26a7505006bd5d9b7df961829b0d414270822aa
helloukey <kunalukey32@gmail.com>
2023-10-11 18:54:04 +0530

    added new app router instance typescript type

--- website_v2/src/types/regsiter.ts
+++ website_v2/src/types/regsiter.ts
@@ -1,4 +1,4 @@
-import {AppRouterInstance} from "next/dist/shared/lib/app-router-context";
+import {AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 type WorkExperience = {
   key?: string;
