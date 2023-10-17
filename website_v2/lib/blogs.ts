536d49f215d5485014f98c35dec330113938baba
helloukey <kunalukey32@gmail.com>
2023-10-17 17:34:50 +0530

    added blogs content with markdown

--- website_v2/lib/blogs.ts
+++ website_v2/lib/blogs.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import {remark} from "remark";
+import html from "remark-html";
 
 const blogsDirectory = path.join(process.cwd(), "blogs");
 
@@ -37,4 +39,31 @@ export function getSortedBlogsData() {
       return -1;
     }
   });
+}
+
+// Get Post Data
+export async function getBlogData(id: string) {
+  const fullPath = path.join(blogsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  // Combine the data with the id
+  return {
+    id,
+    date: matterResult.data.date,
+    title: matterResult.data.title,
+    description: matterResult.data.description,
+    thumbnail: matterResult.data.thumbnail,
+    author: matterResult.data.author,
+    contentHtml,
+    ...matterResult.data,
+  };
 }
--- website_v2/src/app/blogs/[slug]/page.tsx
+++ website_v2/src/app/blogs/[slug]/page.tsx
@@ -1,12 +1,61 @@
-import {getSortedBlogsData} from "../../../../lib/blogs";
+import {notFound} from "next/navigation";
+import {getBlogData, getSortedBlogsData} from "../../../../lib/blogs";
+import Image from "next/image";
+import Link from "next/link";
 
-const Page = async ({params}: {params: {slug: string}}) => {
-  const allBlogsData = await getSortedBlogsData();
-  const blog = allBlogsData.find(blog => blog.id === params.slug);
+// Get static params
+export async function generateStaticParams() {
+  const allBlogsData = getSortedBlogsData();
+  return allBlogsData.map(blog => ({
+    slug: blog.id,
+  }));
+}
+
+// Types
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+const Page = async ({params}: Props) => {
+  const {slug} = params;
+  const blogData = await getBlogData(slug);
+
+  // Not found if no data
+  if (!blogData) notFound();
 
   return (
     <div className="w-full max-w-6xl mx-auto px-4 pb-20">
-      <div className="mt-8">{blog?.description}</div>
+      {/* All Blogs & Title */}
+      <Link href="/blogs" className="btn btn-link normal-case px-0 pt-16">
+        ❮ All Blogs
+      </Link>
+      <h1 className="w-full max-w-6xl mx-auto text-xl text-left sm:text-heading sm:leading-normal font-bold mt-4">
+        {blogData.title}
+      </h1>
+      {/* Header image */}
+      <Image
+        src={blogData.thumbnail}
+        alt={blogData.title}
+        width={1280}
+        height={720}
+        className="mt-4"
+      />
+      {/* Author and Date */}
+      <div className="flex flex-row gap-8 justify-between py-4">
+        <p className="text-xs">
+          Written by: <span className="font-semibold">{blogData.author}</span>
+        </p>
+        <p className="text-xs">{blogData.date}</p>
+      </div>
+      {/* Markdown Content */}
+      <article className="min-w-full prose lg:prose-xl">
+        <div
+          dangerouslySetInnerHTML={{__html: blogData.contentHtml}}
+          className="min-w-full"
+        />
+      </article>
     </div>
   );
 };
