b8fcd21be79b4613e6024c6b04eadd2bc20fdf1e
helloukey <kunalukey32@gmail.com>
2023-10-30 17:59:02 +0530

    added end session email feature for mentee and mentor

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,6 +5,8 @@ config.sendgrid = {
     mentee_signup: 'd-47fe9e3639b34f37965bcb6b9e918f49',
     mentor_signup: 'd-ff0409b50ae84b348b21615ce9c5ac01',
     connection_request: 'd-83945aa2f4d64db79b55d13db4ef4ae3',
+    mentee_session_end: 'd-64fc5a3ab3ea49fe8e21d6823f18c1f2',
+    mentor_session_end: 'd-a7569e82948b4b56ae7766015d2f2259',
   },
 };
 
--- backend/controllers/matchController.js
+++ backend/controllers/matchController.js
@@ -106,118 +106,105 @@ const createMatch = async (req, res) => {
   };
 };
 
-const updateMatch = (req, res) => {
+const updateMatch = async (req, res) => {
   // inputs : matchId
 
-  const { body } = req;
+  try {
+    const { body } = req;
 
-  if (!body) {
-    return res
-      .status(400)
-      .json({ success: false, error: 'request body is empty' });
-  }
+    if (!body) {
+      return res
+        .status(400)
+        .json({ success: false, error: 'request body is empty' });
+    }
 
-  const { matchId, status } = body;
+    const { matchId, status } = body;
 
-  if (!matchId || !status) {
-    return res.status(400).json({
-      success: false,
-      error: 'request body does not have the required parameters',
-    });
-  }
+    if (!matchId || !status) {
+      return res.status(400).json({
+        success: false,
+        error: 'request body does not have the required parameters',
+      });
+    }
 
-  Match.findById(matchId)
-    .exec()
-    .then((match) => {
-      const { mentee, mentor } = match;
-      if (
-        (match.status == 'pending' || match.status == 'closed') &&
-        status === 'active'
-      ) {
-        let requestMessage = '';
-        if (match.status == 'closed') {
-          // reconnecting
-          requestMessage = body.requestMessage;
-        } else {
-          requestMessage = match.requestMessage;
-        }
-
-        createChatConversation(mentee, mentor)
-          .then((chatResult) => {
-            return Session.create({
-              match: match._id,
-              requestMessage,
-              startDate: moment.utc().toDate().toUTCString(),
-              status: 'active',
-              twilioConversationSid: chatResult.conversationSid,
-            }).then((session) => {
-              return Match.findByIdAndUpdate(
-                matchId,
-                {
-                  status,
-                  latestSession: session._id,
-                  requestMessage,
-                },
-                { new: true },
-              )
-                .populate('mentor')
-                .populate('mentee')
-                .populate('latestSession')
-                .exec();
-            });
-          })
-          .then((updatedMatch) => {
-            console.log(updatedMatch);
-            return res.status(200).json({
-              success: true,
-              updatedMatch,
-            });
-          })
-          .catch((e) => {
-            console.log(e);
-            return res.status(500).json({ success: false });
-          });
-      } else if (
-        (match.status == 'active' || match.status == 'pending') &&
-        status == 'closed'
-      ) {
-        const results = {};
-        return Match.findByIdAndUpdate(
-          matchId,
-          {
-            status,
-          },
-          { new: true },
-        )
-          .populate('mentor')
-          .populate('mentee')
-          .populate('latestSession')
-          .exec()
-          .then((updatedMatch) => {
-            results.updatedMatch = updatedMatch;
-            return Session.findByIdAndUpdate(updatedMatch.latestSession, {
-              status,
-              emdDate: moment.utc().toDate().toUTCString(),
-            });
-          })
-          .then((updatedSession) => {
-            return res.status(200).json({
-              success: true,
-              updatedMatch: results.updatedMatch,
-            });
-          })
-          .catch((e) => {
-            console.log(e);
-            return res.status(500).json({ success: false });
-          });
+    const match = await Match.findById(matchId).exec();
+    const { mentee, mentor } = match;
+    if (
+      (match.status == 'pending' || match.status == 'closed') &&
+      status === 'active'
+    ) {
+      let requestMessage = '';
+      if (match.status == 'closed') {
+        // reconnecting
+        requestMessage = body.requestMessage;
       } else {
-        throw new Error('Invalid Request');
+        requestMessage = match.requestMessage;
       }
-    })
-    .catch((e) => {
-      console.log(e);
-      return res.status(500).json({ success: false });
-    });
+
+      const chatResult = await createChatConversation(mentee, mentor);
+
+      const session = await Session.create({
+        match: match._id,
+        requestMessage,
+        startDate: moment.utc().toDate().toUTCString(),
+        status: 'active',
+        twilioConversationSid: chatResult.conversationSid,
+      });
+
+      const updatedMatch = await Match.findByIdAndUpdate(
+        matchId,
+        {
+          status,
+          latestSession: session._id,
+          requestMessage,
+        },
+        { new: true },
+      )
+        .populate('mentor')
+        .populate('mentee')
+        .populate('latestSession')
+        .exec();
+
+      return res.status(200).json({
+        success: true,
+        updatedMatch,
+      });
+    }
+    if (
+      (match.status == 'active' || match.status == 'pending') &&
+      status == 'closed'
+    ) {
+      const updatedMatch = await Match.findByIdAndUpdate(
+        matchId,
+        {
+          status,
+        },
+        { new: true },
+      )
+        .populate('mentor')
+        .populate('mentee')
+        .populate('latestSession')
+        .exec();
+
+      // Send session end email to mentor and mentee 
+      await sendMail(updatedMatch.mentee.email, 'Openmentorship: Session Ended', {}, config.sendgrid.templates.mentee_session_end);
+      await sendMail(updatedMatch.mentor.email, 'Openmentorship: Session Ended', {}, config.sendgrid.templates.mentor_session_end);
+
+      await Session.findByIdAndUpdate(updatedMatch.latestSession, {
+        status,
+        endDate: moment.utc().toDate().toUTCString(),
+      });
+
+      return res.status(200).json({
+        success: true,
+        updatedMatch,
+      });
+    }
+    throw new Error('Invalid Request');
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false });
+  }
 };
 
 const userRecommendations = async (req, res) => {
