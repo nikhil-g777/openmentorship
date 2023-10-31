8b1019210edcfe65a181e38d6d34645da5a29193
helloukey <kunalukey32@gmail.com>
2023-10-31 17:16:36 +0530

    changed review banner button text and color

--- website_v2/src/components/chat/header/review_banner.tsx
+++ website_v2/src/components/chat/header/review_banner.tsx
@@ -41,21 +41,23 @@ const ReviewBanner = () => {
   return (
     <>
       {reviewBanner ? (
-        <div className="w-full p-4 bg-primary flex flex-col sm:flex-row justify-between items-center gap-2 sm:gap-4">
-          {/* Typography */}
-          <div className="text-base-100">
-            <p className="text-base font-semibold">
-              Rate your mentorship session!
-            </p>
-            <p className="text-sm">Leave a review for your mentor.</p>
+        <div className="mx-4">
+          <div className="w-full p-4 bg-primary flex flex-col sm:flex-row justify-between items-center gap-2 sm:gap-4 mt-4 rounded-lg">
+            {/* Typography */}
+            <div className="">
+              <p className="text-base font-semibold">
+                Rate your mentorship session!
+              </p>
+              <p className="text-sm">Leave a review for your mentor.</p>
+            </div>
+            {/* Rate Session Button */}
+            <button
+              className="w-fit btn btn-sm rounded-full normal-case bg-slate-200 text-black border-none lg:btn-md hover:text-white"
+              onClick={() => setMentorReviewModal(true)}
+            >
+              Review Session
+            </button>
           </div>
-          {/* Rate Session Button */}
-          <button
-            className="w-fit btn btn-sm rounded-full normal-case"
-            onClick={() => setMentorReviewModal(true)}
-          >
-            Rate Session
-          </button>
         </div>
       ) : null}
     </>
