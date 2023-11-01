b8254f505ffc36e9dd93df34ccd630c34508886f
helloukey <kunalukey32@gmail.com>
2023-11-01 20:47:20 +0530

    moved server config code to server.js file and changed tempAuth controller condition

--- backend/tests/demo.test.js
+++ backend/tests/demo.test.js
@@ -1,10 +1,24 @@
-const add = () => {
-  return 1 + 2;
-};
+const supertest = require('supertest');
+const app = require('../server');
+const db = require('../db');
 
-describe('testing', () => {
-  test('should add two numbers', () => {
-    const result = add();
-    expect(result).toBe(3);
+describe('app init testing', () => {
+  afterAll(() => db.close());
+  test('should console log', async () => {
+    const response = await supertest(app).post(
+      `/users/tempAuth/${
+        process.env.JEST_ACCOUNT_TYPE === 'mentee'
+          ? process.env.JEST_MENTEE_ID
+          : process.env.JEST_MENTOR_ID
+      }`,
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.message).toBe('Login Successful');
+    expect(response.body.token).toBeDefined();
+    expect(response.body.user).toBeDefined();
+    expect(response.body.user._id).toBeDefined();
+    expect(response.body.user.userType).toBeDefined();
   });
 });
