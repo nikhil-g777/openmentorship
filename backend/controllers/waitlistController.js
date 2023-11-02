95e9a809face7a7314cf3d478f80465679963ce5
helloukey <kunalukey32@gmail.com>
2023-11-02 15:52:56 +0530

    changed waitlistController functions to async await

--- backend/controllers/waitlistController.js
+++ backend/controllers/waitlistController.js
@@ -1,40 +1,37 @@
 const Waitlist = require('../models/waitlist');
 
-const register = (req, res) => {
-  const { body } = req;
+const register = async (req, res) => {
+  try {
+    const { body } = req;
 
-  if (!body) {
-    return res
-      .status(400)
-      .json({ success: false, error: 'request body is empty' });
-  }
+    if (!body) {
+      return res
+        .status(400)
+        .json({ success: false, error: 'request body is empty' });
+    }
 
-  if (!body.waitlist) {
-    return res.status(400).json({
-      success: false,
-      error: 'request body does not have a waitlist object',
-    });
-  }
+    if (!body.waitlist) {
+      return res.status(400).json({
+        success: false,
+        error: 'request body does not have a waitlist object',
+      });
+    }
 
-  const waitlistObj = body.waitlist;
+    const waitlistObj = body.waitlist;
 
-  Waitlist.create(
-    {
+    const createdWaitlist = await Waitlist.create({
       userType: waitlistObj.userType,
       email: waitlistObj.email,
-    },
-    (err, createdWaitlist) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ success: false, error: err });
-      }
+    });
 
-      return res.status(200).json({
-        success: true,
-        waitlist: createdWaitlist,
-      });
-    },
-  );
+    return res.status(200).json({
+      success: true,
+      waitlist: createdWaitlist,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ success: false, error: err });
+  }
 };
 
 module.exports = {
