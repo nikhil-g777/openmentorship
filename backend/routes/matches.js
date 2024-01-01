20c8ff3971fecc013d800b1976270a4d8d4075d4
helloukey <kunalukey32@gmail.com>
2024-01-01 22:11:04 +0530

    returned error for userType mentor
    Change-Id: I32311a10b76c5aff239da4f475614b9eaa8ad874

--- backend/routes/matches.js
+++ backend/routes/matches.js
@@ -9,6 +9,7 @@ require('../config/passportJWT')(passport);
 const matchController = require('../controllers/matchController');
 const util = require('../lib/utils');
 const role = require('../lib/role');
+const errorCodes = require('../lib/errorCodes');
 
 router.use(passport.initialize());
 router.use(cookieParser());
@@ -33,6 +34,17 @@ router.get(
   (req, res, next) => {
     passport.authenticate('jwt', { session: false }, (err, user) => {
       if (err || !user) return next();
+      // Return error if user is mentor
+      if (user.userType === role.mentor) {
+        res
+          .status(401)
+          .json({
+            success: false,
+            statusCode: errorCodes.loginInvalid.code,
+            message: 'Unauthorized',
+          });
+      }
+      // Set user
       req.user = user;
       next();
     })(req, res, next);
@@ -46,6 +58,17 @@ router.get(
   (req, res, next) => {
     passport.authenticate('jwt', { session: false }, (err, user) => {
       if (err || !user) return next();
+      // Return error if user is mentor
+      if (user.userType === role.mentor) {
+        res
+          .status(401)
+          .json({
+            success: false,
+            statusCode: errorCodes.loginInvalid.code,
+            message: 'Unauthorized',
+          });
+      }
+      // Set user
       req.user = user;
       next();
     })(req, res, next);
