c07b7b492b34479bc32b6b4eb324d8549bec7ffc
helloukey <kunalukey32@gmail.com>
2024-01-03 00:56:39 +0530

    fixed layout shift for explore page mentors listing
    Change-Id: Ic53405551a5f8c6e6680998cbfad6675848db092

--- website_v2/src/components/listing/listing_main.tsx
+++ website_v2/src/components/listing/listing_main.tsx
@@ -4,16 +4,34 @@ import {UserProfile} from "@/types/profile";
 import {ProfileCard as Profile} from "../profileCard";
 import {useListingStore} from "@/zustand/store";
 
-const Listing = () => {
+type Props = {
+  data?: UserProfile["user"][];
+  dataHeading?: string;
+};
+
+const Listing = ({data, dataHeading}: Props) => {
   const {listingData, heading} = useListingStore();
 
   return (
     <div className="w-full pt-20 px-4 max-w-6xl mx-auto">
-      {listingData && listingData.length && heading && heading.length ? (
+      {/* Heading */}
+      {data && data.length && dataHeading && dataHeading.length ? (
+        <h3 className="font-lg sm:text-xl font-semibold px-4">{dataHeading}</h3>
+      ) : null}
+      {!data &&
+      listingData &&
+      listingData.length &&
+      heading &&
+      heading.length ? (
         <h3 className="font-lg sm:text-xl font-semibold px-4">{heading}</h3>
       ) : null}
       {/* Results */}
-      {listingData && listingData.length
+      {data && data.length
+        ? data.map((profile: UserProfile["user"]) => (
+            <Profile key={profile._id} data={profile} />
+          ))
+        : null}
+      {!data && listingData && listingData.length
         ? listingData.map((profile: UserProfile["user"]) => (
             <Profile key={profile._id} data={profile} />
           ))
